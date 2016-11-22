0e10687cbf00637b9c16bf9425ee85d9036ccc4f
James <James.Steel@me.com>
2016-11-22 18:40:41 +0000

    another test passing

--- blog/tests/test_sign_up.py
+++ blog/tests/test_sign_up.py
@@ -24,13 +24,24 @@ class NewUserTestCase(TestCase):
     def test_user_registration(self):
         self.driver.implicitly_wait(20)
         self.driver.get("http://127.0.0.1:8000/accounts/register/")
-        self.driver.find_element_by_id('id_username').send_keys('Test42')
+        self.driver.find_element_by_id('id_username').send_keys('Test4211')
         self.driver.find_element_by_id('id_email').send_keys('test@test.com')
         self.driver.find_element_by_id('id_password1').send_keys('Unchained')
         self.driver.find_element_by_id('id_password2').send_keys('Unchained')
         self.driver.find_element_by_id('sign_up').click()
         bodyText = self.driver.find_element_by_id('message').text
         self.assertEqual("Thanks for registering. You are now logged in.", bodyText)
+
+    def test_user_uses_exisiting_name(self):
+        self.driver.implicitly_wait(20)
+        self.driver.get("http://127.0.0.1:8000/accounts/register/")
+        self.driver.find_element_by_id('id_username').send_keys('Test')
+        self.driver.find_element_by_id('id_email').send_keys('test@test.com')
+        self.driver.find_element_by_id('id_password1').send_keys('Unchained')
+        self.driver.find_element_by_id('id_password2').send_keys('Unchained')
+        self.driver.find_element_by_id('sign_up').click()
+        bodyText = self.driver.find_elements_by_class_name('errorlist')[0].text
+        self.assertEqual("A user with that username already exists.", bodyText)
 
 
 
