54c1123519061d97623387d15b50bee0a79c9c93
souljuse <oppo.irene@gmail.com>
2016-11-22 16:55:44 +0100

    testing & factory boy

--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from . import factories
+
+#test: if index has blog posts, if can create a new post,
 
 class BlogTestCase(LiveServerTestCase):
 
@@ -11,7 +14,7 @@ class BlogTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_user_find_posts(self):
+    def test_blog_title(self):
         """
         Test that a user can search for post
         """
@@ -21,3 +24,25 @@ class BlogTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element_by_css_selector('.page-header')
         print(brand_element.text)
         self.assertEqual('Unchained Blog', brand_element.text)
+
+    def test_index_has_posts(self):
+        """
+        Test if index displays the posts
+        """
+        home_page = self.browser.get(self.live_server_url + '/')
+        post = PostFactory()
+        post_element = self.browser.find_element_by_css_selector('.post a')
+        self.assertEqual('Test Title', post_element.text)
+
+    # def test_create_new_posts(self):
+    #     """
+    #     Test if index displays the posts
+    #     """
+    #     # blog home page
+    #     home_page = self.browser.get(self.live_server_url + '/post/new')
+    #
+    #     title_input.send_keys('saxophone')
+    #     instrument_input.submit()
+    #     brand_element = self.browser.find_element_by_css_selector('.page-header')
+    #     print(brand_element.text)
+    #     self.assertEqual('Unchained Blog', brand_element.text)
