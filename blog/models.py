53fda729ec430a00da74a70b5e4c130ecf76bed6
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-22 16:34:13 +0000

    1) Created Comment model and association to Post model 2) Creat passing tests for the creation of comments and association with Post

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.body
---
+++ blog/tests/test_comments.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.db import models
+from blog.models import Comment
+from blog.models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+
+class CommentsTestCase(TestCase):
+    def test_comment_creation(self):
+        me = User.objects.create(username='terry')
+        post = Post.objects.create(author=me, title='A test blog', text="Whatever")
+        comment = Comment.objects.create(author=me, body="great blog!", post=post)
+        self.assertEqual(len(Comment.objects.all()), 1)
+
+    def test_comment_contains_text(self):
+        me = User.objects.create(username='terry')
+        post = Post.objects.create(author=me, title='A test blog', text="Whatever")
+        comment = Comment.objects.create(author=me, body="great blog!", post=post)
+        self.assertEqual(comment.body, "great blog!")
+        self.assertEqual(comment.author, me)
