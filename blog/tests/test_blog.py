5393e3873a70d8eb780588d19ede9b45d596c8b4
souljuse <oppo.irene@gmail.com>
2016-11-23 18:10:02 +0100

    emojis implemented

--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -19,8 +19,8 @@ class BlogTestCase(LiveServerTestCase):
         self.browser.get(self.live_server_url + '/')
 
 
-    def tearDown(self):
-        self.browser.quit()
+    # def tearDown(self):
+    #     self.browser.quit()
 
     def test_blog_title(self):
         title_element = self.browser.find_element_by_css_selector('.page-header')
@@ -47,10 +47,27 @@ class BlogTestCase(LiveServerTestCase):
         self.assertEqual('New post', post_title_element.text)
 
     def register_user(self):
-        self.browser.implicitly_wait(20)
+        # self.browser.implicitly_wait(5)
         self.browser.get(self.live_server_url + '/accounts/register')
         self.browser.find_element_by_id('id_username').send_keys('TestUser')
         self.browser.find_element_by_id('id_email').send_keys('test@test.com')
         self.browser.find_element_by_id('id_password1').send_keys('Unchained')
         self.browser.find_element_by_id('id_password2').send_keys('Unchained')
         self.browser.find_element_by_id('sign_up').click()
+    #
+    # def create_post(self, fff):
+    #     self.browser.find_element_by_id('add-post').click()
+    #     self.browser.find_element_by_id('id_title').send_keys('New post')
+    #     self.browser.find_element_by_id('id_text').send_keys('This is a test post.')
+    #     self.browser.find_element_by_class_name('save').click()
+    #
+    # 
+    # def test_user_can_edit_post(self):
+    #     dd = self.register_user()
+    #     self.browser.get(self.live_server_url + '/post/1/edit')
+    #     self.browser.implicitly_wait(5)
+    #     self.browser.find_element_by_class_name('edit-btn').click()
+    #     self.browser.find_element_by_id('id_title').send_keys('Edited post')
+    #     self.browser.find_element_by_class_name('save').click()
+    #     post_title_element = self.browser.find_element_by_id('post-detail-title')
+    #     self.assertEqual('Edited post', post_title_element.text)
