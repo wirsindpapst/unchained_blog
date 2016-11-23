647515268fd2c980ab52b86653fa7b0dd2598b0d
souljuse <oppo.irene@gmail.com>
2016-11-23 11:38:01 +0100

    has posts test passing

--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -3,6 +3,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from blog.models import Post
 from django.contrib.auth.models import User
+from blog.forms import PostForm
 
 
 #test: if index has blog posts, if can create a new post,
@@ -12,6 +13,7 @@ class BlogTestCase(LiveServerTestCase):
     def setUp(self):
         self.user = User.objects.create(username="jen")
         self.post = Post.objects.create(author=self.user, title="Test title", text="Hello world!")
+        self.post.publish()
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(2)
 
@@ -19,35 +21,20 @@ class BlogTestCase(LiveServerTestCase):
         self.browser.quit()
 
     def test_blog_title(self):
-        """
-        Test that a user can search for post
-        """
-        # blog home page
         home_page = self.browser.get(self.live_server_url + '/')
-        # site heading
         brand_element = self.browser.find_element_by_css_selector('.page-header')
         print(brand_element.text)
         self.assertEqual('Unchained Blog', brand_element.text)
 
     def test_index_has_posts(self):
-        """
-        Test if index displays the posts
-        """
         home_page = self.browser.get(self.live_server_url + '/')
-        print(self.post.title)
         post_element = self.browser.find_element_by_css_selector('.post-title')
-        self.assertEqual(post.title, post_element.text)
+        self.assertEqual(self.post.title, post_element.text)
 
+    def test_valid_form(self):
+        w = Post.objects.create(title='Foo', text='Bar', author_id=self.user.id)
+        data = {'title': w.title, 'text': w.text, 'author_id': w.author_id}
+        form = PostForm(data=data)
+        self.assertTrue(form.is_valid())
 
-    # def test_create_new_posts(self):
-    #     """
-    #     Test if index displays the posts
-    #     """
-    #     # blog home page
-    #     home_page = self.browser.get(self.live_server_url + '/post/new')
-    #
-    #     title_input.send_keys('saxophone')
-    #     instrument_input.submit()
-    #     brand_element = self.browser.find_element_by_css_selector('.page-header')
-    #     print(brand_element.text)
-    #     self.assertEqual('Unchained Blog', brand_element.text)
+    
