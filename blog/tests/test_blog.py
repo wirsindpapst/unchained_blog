a7c9dd01ba418da7805ef46d3b8c7d098b328a3b
hanrattyjen <hanratty.jen@gmail.com>
2016-11-23 11:27:25 +0000

    User can add a post with title and text. Tests pass.

--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -16,15 +16,15 @@ class BlogTestCase(LiveServerTestCase):
         self.post.publish()
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(2)
+        self.browser.get(self.live_server_url + '/')
+
 
     def tearDown(self):
         self.browser.quit()
 
     def test_blog_title(self):
-        home_page = self.browser.get(self.live_server_url + '/')
-        brand_element = self.browser.find_element_by_css_selector('.page-header')
-        print(brand_element.text)
-        self.assertEqual('Unchained Blog', brand_element.text)
+        title_element = self.browser.find_element_by_css_selector('.page-header')
+        self.assertEqual('Unchained Blog', title_element.text)
 
     def test_index_has_posts(self):
         home_page = self.browser.get(self.live_server_url + '/')
@@ -36,3 +36,19 @@ class BlogTestCase(LiveServerTestCase):
         data = {'title': w.title, 'text': w.text, 'author_id': w.author_id}
         form = PostForm(data=data)
         self.assertTrue(form.is_valid())
+
+    def test_user_can_add_post(self):
+        self.browser.implicitly_wait(20)
+        self.browser.get(self.live_server_url + '/accounts/register')
+        self.browser.find_element_by_id('id_username').send_keys('TestUser')
+        self.browser.find_element_by_id('id_email').send_keys('test@test.com')
+        self.browser.find_element_by_id('id_password1').send_keys('Unchained')
+        self.browser.find_element_by_id('id_password2').send_keys('Unchained')
+        self.browser.find_element_by_id('sign_up').click()
+        self.browser.find_element_by_class_name('top-menu').click()
+        self.browser.find_element_by_id('id_title').send_keys('New post')
+        self.browser.find_element_by_id('id_text').send_keys('This is a test post.')
+        self.browser.find_element_by_class_name('save').click()
+        # print(self.browser.find element ecc(body))
+        post_title_element = self.browser.find_element_by_id('post-detail-title')
+        self.assertEqual('New post', post_title_element.text)
