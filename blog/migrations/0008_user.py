28d218cb4352a2c77edebd4f0d0effafa361caf1
James <James.Steel@me.com>
2016-11-25 19:07:40 +0000

    button disabled

--- blog/migrations/0008_user.py
+++ blog/migrations/0008_user.py
@@ -11,11 +11,4 @@ class Migration(migrations.Migration):
         ('blog', '0007_merge_20161124_1528'),
     ]
 
-    operations = [
-        migrations.CreateModel(
-            name='User',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-            ],
-        ),
-    ]
+    
